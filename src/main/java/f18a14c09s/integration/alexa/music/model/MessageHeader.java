eac1e9bd8a712e3dabfd4358cf1170dc90bcec32
Francis Johnson <fjohnson@eab.com>
2018-12-06 11:37:37 -0500

    Added a few entities and created JSON deserialization test framework.

---
+++ src/main/java/f18a14c09s/integration/alexa/music/model/MessageHeader.java
@@ -0,0 +1,22 @@
+package f18a14c09s.integration.alexa.music.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MessageHeader {
+
+    private String messageId;
+
+    // TODO: Deserialization will fail on this field until it is annotated or written with custom deserialization.
+    private MessageNamespace namespace;
+
+    private Enum<?> name;
+
+    private String payloadVersion;
+
+}
