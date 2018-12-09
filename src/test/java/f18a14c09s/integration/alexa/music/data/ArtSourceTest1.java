180f9ddccaf49f47649f25c8b24d4ccdf70ad7b9
Francis Johnson <business@francisjohnson.org>
2018-12-09 06:35:25 -0500

    Reformatted.

--- src/test/java/f18a14c09s/integration/alexa/music/data/ArtSourceTest1.java
+++ src/test/java/f18a14c09s/integration/alexa/music/data/ArtSourceTest1.java
@@ -1,17 +1,28 @@
 package f18a14c09s.integration.alexa.music.data;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+
 import java.io.IOException;
 import java.util.*;
 import java.util.function.*;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 /**
- * 
+ *
  */
-public class ArtSourceTest1 {public static final String TEST_CASE = "{   \"url\": \"https://example.com/images/cover/48x48-000000-80-0-0.jpg\",   \"size\": \"X_SMALL\",   \"widthPixels\": 48,   \"heightPixels\": 48 }";
-@Test
-void testDeserialization() throws IOException {
-ObjectMapper jsonMapper = new ObjectMapper();ArtSource subject = jsonMapper.readValue(TEST_CASE, ArtSource.class);
-assertEquals(subject.getSize(), "X_SMALL");assertEquals(((Number)subject.getHeightPixels()).longValue(), 48L);assertEquals(subject.getUrl(), "https://example.com/images/cover/48x48-000000-80-0-0.jpg");assertEquals(((Number)subject.getWidthPixels()).longValue(), 48L);
-}
+public class ArtSourceTest1 {
+    public static final String TEST_CASE =
+            "{   \"url\": \"https://example.com/images/cover/48x48-000000-80-0-0.jpg\",   \"size\": \"X_SMALL\",   \"widthPixels\": 48,   \"heightPixels\": 48 }";
+
+    @Test
+    void testDeserialization() throws IOException {
+        ObjectMapper jsonMapper = new ObjectMapper();
+        ArtSource subject = jsonMapper.readValue(TEST_CASE, ArtSource.class);
+        assertEquals(subject.getSize(), "X_SMALL");
+        assertEquals(((Number) subject.getHeightPixels()).longValue(), 48L);
+        assertEquals(subject.getUrl(), "https://example.com/images/cover/48x48-000000-80-0-0.jpg");
+        assertEquals(((Number) subject.getWidthPixels()).longValue(), 48L);
+    }
 }
