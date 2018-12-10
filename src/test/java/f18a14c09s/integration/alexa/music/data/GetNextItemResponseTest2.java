e829edd758584e4c8e9495a9fb0afb2e15df8e5f
Francis Johnson <business@francisjohnson.org>
2018-12-10 00:11:48 -0500

    Regenerated the message unit tests.  Need to work through all of the issues, now.

---
+++ src/test/java/f18a14c09s/integration/alexa/music/data/GetNextItemResponseTest2.java
@@ -0,0 +1,19 @@
+package f18a14c09s.integration.alexa.music.data;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.util.*;
+import java.util.function.*;
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * When the skill determines that the user has reached their daily skip limit, it returns an error response indicating the error case as in the following example.
+ */
+public class GetNextItemResponseTest2 {public static final String TEST_CASE = "{   \"header\": {     \"messageId\": \"2cae4d53-6bc1-4f8f-aa98-7dd2727ca84b\",     \"namespace\": \"Alexa.Audio\",     \"name\": \"ErrorResponse\",     \"payloadVersion\": \"1.0\"   },   \"payload\": {     \"type\": \"SKIP_LIMIT_REACHED\",     \"message\": \"The user has reached their daily skip limit.\",     \"retryPeriod\": \"DAILY\"   } }";
+@Test
+void testDeserialization() throws IOException {
+ObjectMapper jsonMapper = new ObjectMapper();Response obj = jsonMapper.readValue(TEST_CASE, Response.class);GetNextItemResponse subject = (GetNextItemResponse)obj;
+
+assertEquals(subject.getPayload().getType(), "SKIP_LIMIT_REACHED");assertEquals(subject.getPayload().getMessage(), "The user has reached their daily skip limit.");assertEquals(subject.getPayload().getRetryPeriod(), "DAILY");
+assertEquals(subject.getHeader().getMessageId(), "2cae4d53-6bc1-4f8f-aa98-7dd2727ca84b");assertEquals(subject.getHeader().getNamespace(), "Alexa.Audio");assertEquals(subject.getHeader().getName(), "ErrorResponse");assertEquals(subject.getHeader().getPayloadVersion(), "1.0");
+}
+}
