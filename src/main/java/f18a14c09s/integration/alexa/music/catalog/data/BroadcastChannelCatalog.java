7d2aaf2fc6c4d6603f5eee8f839653161972e1b2
Francis Johnson <business@francisjohnson.org>
2018-12-11 02:40:04 -0500

    Added catalog classes.

---
+++ src/main/java/f18a14c09s/integration/alexa/music/catalog/data/BroadcastChannelCatalog.java
@@ -0,0 +1,14 @@
+package f18a14c09s.integration.alexa.music.catalog.data;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class BroadcastChannelCatalog extends AbstractCatalog<Station> {
+    public BroadcastChannelCatalog() {
+        super(null, null);
+    }
+}
---
+++ src/main/java/f18a14c09s/integration/alexa/music/catalog/data/MusicPlaylistCatalog.java
@@ -0,0 +1,14 @@
+package f18a14c09s.integration.alexa.music.catalog.data;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MusicPlaylistCatalog extends AbstractCatalog<Playlist> {
+    public MusicPlaylistCatalog() {
+        super(null, null);
+    }
+}
